3bcb891a3797d00ef26f3293fb1455e3522601c3
Tanner Smith <tanner@tssoftware.net>
2012-07-11 18:36:16 +0200

    Add dynamic creation of coins.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 
-var States = {
+const States = {
 	Start : 0,
 }
 
+const COIN_PADDING = 20;
+
+const COIN_VALUES = [5, 2];
+const START_COIN_QUANTITY = 10;
+
 var state = States.Start;
 var coinsInDish = 0;
-const COIN_PADDING = 20;
 
 function refresh() {
 	var coins = numberOfCoinsInDish();
@@ -36,18 +40,30 @@ function layout() {
 				at: "center center"
 			});
 
-			for (var i = 0; i < $(".coin").length; i++) {
-				$(".coin").eq(i).position({
-					of: $("#left"),
-					my: "left bottom",
-					at: "left bottom",
-					offset: ((i + 1) * ($(".coin").width() + COIN_PADDING)) - $(".coin").width() + " -" + COIN_PADDING,
-				});
+			for (var i = 0; i < COIN_VALUES.length; i++) {
+				for (var j = 0; j < START_COIN_QUANTITY; j++) {
+					var coin = $("<div>", {
+						"class": "coin",
+					});
+
+					coin.attr("data-value", COIN_VALUES[i]).appendTo("#left");
+				}
 			}
+
+			// for (var i = 0; i < $(".coin").length; i++) {
+			// 	$(".coin").eq(i).position({
+			// 		of: $("#left"),
+			// 		my: "left bottom",
+			// 		at: "left bottom",
+			// 		offset: ((i + 1) * ($(".coin").width() + COIN_PADDING)) - $(".coin").width() + " -" + COIN_PADDING,
+			// 	});
+			// }
 	}
 }
 
 $(document).on("ready", function(e) {
+	layout();
+
 	$(".coin").draggable({
 		stack: ".coin",
 		containment: "parent",
@@ -59,6 +75,4 @@ $(document).on("ready", function(e) {
 			refresh();
 		}
 	});
-
-	layout();
 });
