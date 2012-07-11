89fbc4ddd8b8b0cae61e8c31dfc2da25519ec56f
Tanner Smith <tanner@tssoftware.net>
2012-07-11 18:00:53 +0200

    Create functionality to count coins in dish. Restyle things.

--- main.js
+++ main.js
@@ -4,9 +4,27 @@ var States = {
 }
 
 var state = States.Start;
+var coinsInDish = 0;
+const COIN_PADDING = 20;
 
 function refresh() {
+	var coins = numberOfCoinsInDish();
+	if (coins != coinsInDish) {
+		alert(coins + " coin" + (coins != 1 ? "s" : "") + " in the dish");
+		coinsInDish = coins;
+	}
+}
 
+function numberOfCoinsInDish() {
+	var coins = 0;
+
+	$(".coin").each(function(e) {
+		if (new $.rect($(this)).intersects($("#dish"))) {
+			coins++;
+		}
+	});
+
+	return coins;
 }
 
 function layout() {
@@ -23,7 +41,7 @@ function layout() {
 					of: $("#left"),
 					my: "left bottom",
 					at: "left bottom",
-					offset: ((i + 1) * ($(".coin").width() + 25)) - $(".coin").width() + " -25",
+					offset: ((i + 1) * ($(".coin").width() + COIN_PADDING)) - $(".coin").width() + " -" + COIN_PADDING,
 				});
 			}
 	}
@@ -32,11 +50,12 @@ function layout() {
 $(document).on("ready", function(e) {
 	$(".coin").draggable({
 		stack: ".coin",
-		containment: "parent"
-	});
-
-	$(".dish").droppable({
-		drop: function() {
+		containment: "parent",
+		drag: function(event, ui) {
+			$(this).addClass("darkshadow");
+		},
+		stop: function(event, ui) {
+			$(this).removeClass("darkshadow");
 			refresh();
 		}
 	});
