c573634c7837a34777bf1f5dac650fab3919c0cb
Tanner Smith <tanner@tssoftware.net>
2012-07-12 18:53:22 +0200

    Implement states system. Init sample states and make layout dynamic.

--- main.js
+++ main.js
@@ -1,21 +1,106 @@
-
-const States = {
-	Start : 0,
-}
-
 const COIN_PADDING = 20;
 
 const COIN_VALUES = [3, 4];
 const START_COIN_QUANTITY = 5;
 
-var state = States.Start;
+var currentState = null;
 var currentStamp = null;
 
 var coinsInDish = [];
 
 $(document).on("ready", function(e) {
+	initStates();
+
 	layout();
 });
+
+function initStates() {
+	var prevState = null;
+	var state = new State();
+	state.introMessage = "Pay for a 8 cent stamp with 3 and 4 cent coins.";
+	state.rejectMessage = "That is not right, try again.";
+	state.stampPrice = 8;
+	state.validationMethod = function() {
+		return getValueOfCoins() == state.stampPrice;
+	}
+	state.nextState = prevState;
+
+	prevState = state;
+	state = new State();
+	state.introMessage = "Pay for a 7 cent stamp with 3 and 4 cent coins.";
+	state.rejectMessage = "That is not right, try again.";
+	state.stampPrice = 7;
+	state.validationMethod = function() {
+		return getValueOfCoins() == state.stampPrice;
+	}
+	state.nextState = prevState;
+
+	prevState = state;
+	state = new State();
+	state.introMessage = "Pay for a 6 cent stamp with 3 and 4 cent coins.";
+	state.rejectMessage = "That is not right, try again.";
+	state.stampPrice = 6;
+	state.validationMethod = function() {
+		return getValueOfCoins() == state.stampPrice;
+	}
+	state.nextState = prevState;
+
+	currentState = state;
+}
+
+function layout() {
+	$("#dish").position({
+		of: $("#left"),
+		my: "center center",
+		at: "center center"
+	});
+
+	for (var i = 0; i < COIN_VALUES.length; i++) {
+		for (var j = 0; j < START_COIN_QUANTITY; j++) {
+			var coin = createCoin(COIN_VALUES[i]);
+			var stack = $("#dish");
+
+			$(".stack").each(function() {
+				if (parseInt($(this).attr("data-value")) == COIN_VALUES[i]) {
+					stack = $(this);
+					return;
+				}
+			});
+
+			coin.appendTo(stack);
+
+			coin.position({
+				of: stack,
+				my: "center center",
+				at: "center center"
+			});
+
+			coin.css("visibility", "hidden");
+		}
+	}
+
+	setCurrentStamp(createStamp(currentState.stampPrice));
+
+	showTopCoinInStacks();
+}
+
+function refresh() {
+	var coins = getCoinsInDish();
+
+	if (coins != coinsInDish) {
+		var quantity = coins.length;
+		var value = getValueOfCoins(coins);
+
+		console.log(quantity + " coin" + (quantity != 1 ? "s" : "") + " in " +
+			"the dish for a total of " + value + " cents.");
+		console.log("You " + (canPurchaseStamp(currentStamp) ? "can" : "cannot") + 
+			" purchase the current stamp.");
+		console.log("Cost of current stamp: " + currentStamp.attr("data-cost") +
+			" cents.");
+
+		coinsInDish = coins;
+	}
+}
 
 function showTopCoinInStacks() {
 	$(".stack").each(function() {
@@ -27,67 +112,6 @@ function showTopCoinInStacks() {
 
 		coinsInStack.first().css("visibility", "visible");
 	});
-}
-
-function layout() {
-	switch (state) {
-		case States.Start:
-			$("#dish").position({
-				of: $("#left"),
-				my: "center center",
-				at: "center center"
-			});
-
-			for (var i = 0; i < COIN_VALUES.length; i++) {
-				for (var j = 0; j < START_COIN_QUANTITY; j++) {
-					var coin = createCoin(COIN_VALUES[i]);
-					var stack = $("#dish");
-
-					$(".stack").each(function() {
-						if (parseInt($(this).attr("data-value")) == COIN_VALUES[i]) {
-							stack = $(this);
-							return;
-						}
-					});
-
-					coin.appendTo(stack);
-
-					coin.position({
-						of: stack,
-						my: "center center",
-						at: "center center"
-					});
-
-					coin.css("visibility", "hidden");
-				}
-			}
-
-			setCurrentStamp(createStamp(6));
-	}
-
-	showTopCoinInStacks();
-}
-
-function refresh() {
-	var coins = getCoinsInDish();
-
-	if (coins != coinsInDish) {
-		var quantity = coins.length;
-		var value = 0;
-
-		for (var i = 0; i < coins.length; i++) {
-			value += coins[i];
-		}
-
-		console.log(quantity + " coin" + (quantity != 1 ? "s" : "") + " in " +
-			"the dish for a total of " + value + " cents.");
-		console.log("You " + (canPurchaseStamp(currentStamp) ? "can" : "cannot") + 
-			" purchase the current stamp.");
-		console.log("Cost of current stamp: " + currentStamp.attr("data-cost") +
-			" cents.");
-
-		coinsInDish = coins;
-	}
 }
 
 function getCoinsInDish() {
@@ -119,6 +143,16 @@ function canPurchaseStamp(stamp) {
 	}
 
 	return value == stamp.attr("data-cost");
+}
+
+function getValueOfCoins(coins) {
+	var value = 0;
+
+	for (var i = 0; i < coins.length; i++) {
+		value += coins[i];
+	}
+
+	return value;
 }
 
 function setCurrentStamp(stamp) {
@@ -167,4 +201,14 @@ function createStamp(cost) {
 	stamp.appendTo("#left");
 
 	return stamp;
+}
+
+function State() {
+	this.introMessage = "";
+	this.rejectMessage = "";
+
+	this.validationMethod = null;
+	this.stampPrice = 0;
+
+	this.nextState = null;
 }
