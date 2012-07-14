738c0a979846be7ea6307db45700cb033529c4af
Tanner Smith <tanner@tssoftware.net>
2012-07-14 15:39:24 +0200

    Add states for 10 and 11 cent stamps.

--- main.js
+++ main.js
@@ -41,7 +41,9 @@ $(document).on("keyup", function(e) {
 });
 
 function initStates() {
-	var nineCentComboState = createComboState(9, [3], [6], null);
+	var elevenCentComboState = createComboState(11, [3], [8], null);
+	var tenCentComboState = createComboState(10, [3], [7], elevenCentComboState);
+	var nineCentComboState = createComboState(9, [3], [6], tenCentComboState);
 	var eightCentState = createCoinState(8, nineCentComboState);
 	var sevenCentState = createCoinState(7, eightCentState);
 	var sixCentState = createCoinState(6, sevenCentState);
